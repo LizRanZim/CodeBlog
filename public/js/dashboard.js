49d91e890fc0563501a47ec7befb6f9a8f4404b3
LizRanZim <lrzclass22@gmail.com>
2022-11-16 21:46:43 -0500

    Fix update and delete buttons on dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -26,10 +26,9 @@ const delButtonHandler = async (event) => {
     } else {
       alert('Failed to delete post');
     }
-  } else
- {
-console.log('******')
- }
+  } else {
+    console.log('******')
+  }
 };
 
 // Idea is to hide the edit post functionality until someone clicks the edit post button and then they can edit and save the post from here. But this probably needs to move to the dashboard instead from the post handlebars
@@ -37,71 +36,86 @@ console.log('******')
 
 
 // event handler for edit post to unhide the edit area
-const editPostButtonHandler = async (event) => {
+const editPostButtonHandler = (event) => {
   event.preventDefault();
-    // remove the class that hides the element
-    document.querySelector('.edit-post-option')
+
+  const id = event.target.getAttribute('data-id');
+  // remove the class that hides the element
+  document.getElementById(id)
     .classList.remove('display-none');
-  };
+};
 
 
 // event handler to save edited post button
+// ****this is creating a new post instead of saving the edited post
 const saveEditedPostFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const post_id = event.target.getAttribute('data-id');
-    const post_name = document.querySelector('#post-name').value.trim();
+  const id = event.target.getAttribute('data-id');
 
-    const post_description = document.querySelector('#post-description').value.trim();
+  const post_name = document.querySelector('#post-name').value.trim();
+
+  const post_description = document.querySelector('#post-description').value.trim();
 
 
-    // if post-text exists
-    if (post_name && post_description) {
-        const postBody = {
-            post_name: post_name,
-            post_description: post_description,
-            post_id: post_id
-        }
-
-        const response = await fetch('../api/posts', {
-            method: 'POST',
-            body: JSON.stringify(postBody),
-            headers: { 'Content-Type': 'application/json' },
-        });
-        console.log(response)
-
-        if (response.ok) {
-            console.log(response);
-            document.location.replace('/dashboard');
-        } else {
-            alert('Failed to edit post');
-        }
+  // if post-text exists
+  if (post_name && post_description) {
+    const postBody = {
+      post_name: post_name,
+      post_description: post_description,
+      post_id: id,
     }
+
+    const response = await fetch(`../api/posts/${id}`, {
+      method: 'POST',
+      body: JSON.stringify(postBody),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    console.log(postBody)
+
+    if (response.ok) {
+      console.log(response);
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to edit post');
+    }
+  }
+
 };
 
+const testFunction = async (event) => {
+  event.preventDefault();
+  console.log(event.target);
+};
 
 
 // save edited post event listener
 
 document
-    .querySelector('#save-post-btn')
-    .addEventListener('click', saveEditedPostFormHandler);
+  .querySelector('#save-post-btn')
+  .addEventListener('click', saveEditedPostFormHandler);
 
 
 // delete button event listener
 
+// document
+//   .querySelector('.delete-post-btn')
+//   .addEventListener('click', delButtonHandler);
+
+document.querySelectorAll('.delete-post-btn').forEach(e => e.addEventListener('click', delButtonHandler));
+
+
+
 document
-  .querySelector('.delete-post-btn')
-  .addEventListener('click', delButtonHandler);
+  .querySelector('#test')
+  .addEventListener('click', testFunction);
 
-
- // add button event listener
+// add button event listener
 
 document
   .querySelector('#make-post')
   .addEventListener('click', addPostButtonHandler);
 
-  // edit post button event listener
+// edit post button event listener
 document
-.querySelector('#edit-post-btn')
-.addEventListener('click', editPostButtonHandler);
+  .querySelectorAll('.edit-post-btn').forEach(e => e.addEventListener('click', editPostButtonHandler));
